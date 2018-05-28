ba0953b423631abbc3c73a94d10705bd8bcfec57
Zharkov Andrey <zharkov.php@gmail.com>
2018-05-28 22:38:36 +0300

    Компонентная отправка Gmail, настройка шаблона письма и работа с ним

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -5,13 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Mail;
+use App\mail\newMail;
 class MailController extends Controller
 {
     public function send()
     {
-        Mail::send(['text' => 'mail'], ['name', 'Web dev blog'], function ($message){
-            $message->to('zharkov.php@gmail.com', 'To web dev blog')->subject('Test email');
-            $message->from('zharkov.php@gmail.com', 'Web dev blog');
-    });
+       // Mail::send(['text' => 'mail'], ['name', 'Web dev blog'], function ($message){
+       //     $message->to('zharkov.php@gmail.com', 'To web dev blog')->subject('Test email');
+        //    $message->from('zharkov.php@gmail.com', 'Web dev blog');
+  //  });
+
+        Mail::send(new newMail());
+    }
+
+    public function email()
+    {
+        return view('email');
     }
 }
